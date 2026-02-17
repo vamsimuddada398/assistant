c106bd66f42891a0787ba0fba58550a360237439
Muddada Vamsi <vamsimuddada398@gmail.com>
2026-02-17 10:12:55 +0530

    first commit

--- schemas.py
+++ schemas.py
@@ -1,12 +1,33 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,field_validator
 from datetime import date, datetime
+from dateutil import parser
 
 class UserCreate(BaseModel):
     name: str
     dateofbirth: date
+    
+    @field_validator("dateofbirth", mode="before")
+    @classmethod
+    def parse_dob(cls, value):
+        if isinstance(value, date):
+            return value
+        try:
+            return parser.parse(value, dayfirst=True).date()
+        except Exception:
+            raise ValueError("Invalid date format")
 
 
 class AppointmentCreate(BaseModel):
     user_id: int
     appointment_date: datetime
     reason: str
+    
+    @field_validator("appointment_date", mode="before")
+    @classmethod
+    def parse_appointment_date(cls, value):
+        if isinstance(value, datetime):
+            return value
+        try:
+            return parser.parse(value, fuzzy=True)
+        except Exception:
+            raise ValueError("Invalid appointment date format")
